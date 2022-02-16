f0b0d9914e0a5d5625920efb730d82eb16788c52
mistrypragnesh40 <47309472+mistrypragnesh40@users.noreply.github.com>
2022-02-16 11:41:34 +0530

    icon & otp control change added

--- OTPControl/MainPage.xaml.cs
+++ OTPControl/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OTPControl.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace OTPControl
         public MainPage()
         {
             InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
         }
     }
 }
---
+++ OTPControl/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace OTPControl.ViewModels
+{
+    public class MainPageViewModel :BaseViewModel
+    {
+        private string _selectedOTP;
+        public string SelectedOTP
+        {
+            get => _selectedOTP;
+            set => SetProperty(ref _selectedOTP, value);
+
+        }
+
+        public MainPageViewModel()
+        {
+
+        }
+
+
+        public ICommand GetOtpCommand => new Command<string>((param) =>
+         {
+             SelectedOTP = param;
+         });
+    }
+}
